10080cdd0ee75ed32617de4c503abd1bf8b2dfe0
Xiaotangyuaner <42474583+qiliu6767@users.noreply.github.com>
2018-10-08 18:22:08 -0500

    Add files via upload
    Read from the triplets.txt file and generate the corresponding data loader. Note that each element in the data loader is list containing 3 elements (because there is respectively a query, positive and negative image), each of which is a tensor, of size (batch_size * 3(for RGB) * 32(for Image size) * 32).

---
+++ ImageGenerator.py
@@ -0,0 +1,97 @@
+import torch.utils.data as data
+
+from PIL import Image
+import os
+import os.path
+import torchvision.transforms as transforms
+
+def default_loader(path):
+	return Image.open(path).convert('RGB')
+
+def default_flist_reader(flist):
+	imlist = []
+	with open(flist, 'r') as rf:
+		for line in rf.readlines():
+			query, pos, neg = line.strip().split(',')
+			imlist.append((query, pos, neg))
+	return imlist
+
+class ImageFilelist(data.Dataset):
+	def __init__(self,flist, 
+				 query_transform = None, 
+				 pos_transform = None,
+				 neg_transform = None,
+				 flist_reader = default_flist_reader, 
+				 loader = default_loader):
+		"""
+		Args:
+			root: where the images are stored
+			flist: where the text file containing the names of images is stored
+		"""
+		self.imlist = flist_reader(flist)		
+		self.query_transform = query_transform
+		self.pos_transform = pos_transform
+		self.neg_transform = neg_transform
+		self.loader = loader
+
+	def __getitem__(self, index):
+		query, pos, neg = self.imlist[index]
+		query = self.loader(os.path.join(query))
+		pos = self.loader(os.path.join(pos))
+		neg = self.loader(os.path.join(neg))
+		if self.query_transform is not None:
+			query = self.query_transform(query)
+		if self.pos_transform is not None:
+			pos = self.pos_transform(pos)
+		if self.neg_transform is not None:
+			neg = self.neg_transform(neg)
+		
+		return query, pos, neg
+
+	def __len__(self):
+		return len(self.imlist)
+
+train_transform = transforms.Compose([
+	transforms.RandomHorizontalFlip(),
+	transforms.RandomCrop(32, padding = 4),
+	# Convert the images into a format usable by PyTorch
+	transforms.ToTensor(), 
+	# Make all pixels range between -1 to +1
+	transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+trainloader = data.DataLoader(
+	ImageFilelist(flist = '/Users/qiliu/Desktop/Fall2018/IE534/Homeworks/Homework05/triplets.txt',
+			      query_transform = train_transform,
+				  pos_transform = train_transform,
+				  neg_transform = train_transform),
+	batch_size = 10,
+	shuffle = False,
+	num_workers = 4,
+	pin_memory = True)
+
+# # Test 
+# print(list(trainloader)[999][2].size())
+# flist = '/Users/qiliu/Desktop/Fall2018/IE534/Homeworks/Homework05/triplets.txt'
+# test_imlist = default_flist_reader(flist)
+# print(test_imlist[9999])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
