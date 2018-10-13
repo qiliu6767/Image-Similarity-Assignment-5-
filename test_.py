2b795bbc3cbfab300c5e031a3dc2075b61efa022
Xiaotangyuaner <42474583+qiliu6767@users.noreply.github.com>
2018-10-13 17:35:44 -0500

    Update test_.py

--- test_.py
+++ test_.py
@@ -43,19 +43,19 @@ def test(model, train_loader, val_loader,
 		# Convert the validation embedding into a numpy
 		image_emb = image_emb.detach().numpy()
 
-		# Step3: Convert the validation embedding numpy to into the same shape with training image embeddings
+		# Convert the validation embedding numpy to into the same shape with training image embeddings
 		image_emb = np.tile(image_emb, (num_images, 1))
 
-		# Step 4: Calculate the difference
+		# Calculate the difference
 		diff_emb = image_emb - train_images_emb
 
-		# Step 5: Calculate L2 Norm for each row
+		# Calculate L2 Norm for each row
 		l2norm_emb = np.linalg.norm(diff_emb, ord = 2, axis = 1)
 
-		# Step 6: Get the smallest 100 l2norm
+		# Get the smallest 100 l2norm
 		top_similar = l2norm_emb.argsort()[:100]
 
-		# Step 7: Check the how many of these top similar images 
+		# Check the how many of these top similar images 
 		# are in the same class with this current validation image
 		val_label = val_image_labels[i]
 		acc += sum(train_image_labels[top_similar] == val_label) / 100
